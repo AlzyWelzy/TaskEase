320a77fe2aa79287c586632bc59aa49bec9a37fe
AlzyWelzy <welzyalzy@gmail.com>
2023-12-03 14:31:11 +0530

    .

--- app/models.py
+++ app/models.py
@@ -4,3 +4,4 @@ from django.db import models
 # Create your models here.
 class App(models.Model):
     name = models.CharField(max_length=100)
+    progress = models.CharField(max_length=100)
--- app/views.py
+++ app/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import datetime
+from .models import App
 
 
 # Create your views here.
 def index(request):
-    context = {"title": "Taskify", "year": datetime.datetime.now().year}
+    if request.method == "POST":
+        app_name = request.POST.get("app_name")
+        app_progress = request.POST.get("app_progress")
+        print(app_name, app_progress)
+
+        app = App()
+        app.name = app_name
+        app.progress = app_progress
+        app.save()
+
+        return redirect("/")
+
+    queryset = App.objects.all()
+
+    context = {
+        "title": "Taskify",
+        "apps": queryset,
+        "year": datetime.datetime.now().year,
+    }
     return render(request, "index.html", context)
