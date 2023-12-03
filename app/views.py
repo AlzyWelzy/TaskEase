d93d952b62bfc99313def48b7bf1076a0a484370
AlzyWelzy <welzyalzy@gmail.com>
2023-12-03 10:56:27 +0530

    settings

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+import datetime
 
 
 # Create your views here.
 def index(request):
-    context = {"title": "Taskify"}
+    context = {"title": "Taskify", "year": datetime.datetime.now().year}
     return render(request, "index.html", context)
